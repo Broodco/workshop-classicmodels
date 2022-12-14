d58e0e201a7cc4787253626eba351e38f293a684
Broodco <broodcoorens.maxime@gmail.com>
2022-12-14 16:34:29 +0100

    Workshop update 9 : add composer autoloader

--- src/public/index.php
+++ src/public/index.php
@@ -3,8 +3,7 @@ declare(strict_types=1);
 
 session_start();
 
-require_once 'classes/ProductController.php';
-require_once 'classes/AuthController.php';
+require_once 'vendor/autoload.php';
 
 $url = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 
