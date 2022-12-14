fc71f439a777019c9ca30bef4070f71ecbeaeda6
Broodco <broodcoorens.maxime@gmail.com>
2022-12-14 16:22:31 +0100

    Workshop update 8 : Super router test

--- src/public/index.php
+++ src/public/index.php
@@ -4,6 +4,16 @@ declare(strict_types=1);
 session_start();
 
 require_once 'classes/ProductController.php';
+require_once 'classes/AuthController.php';
 
-$productController = new ProductController();
-$productController->index();
+$url = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+
+if ($url === 'login') {
+    $authController = new AuthController();
+    $authController->showLoginForm();
+}
+
+if ($url === '') {
+    $productController = new ProductController();
+    $productController->index();
+}
